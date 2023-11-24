955bde716cfbe2742ebc7b393996563693e52da0
Luan Nadaletti <luannadaletti6@gmail.com>
2023-11-24 14:14:30 -0300

    :bento: improved player sprite

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,17 +7,20 @@ func _process(delta: float) -> void:
 	var direction = Vector2.ZERO
 	
 	if Input.is_action_pressed("move_down"):
+		$AnimatedPlayerSprite.play("frente")
 		direction.y += 1
 	if Input.is_action_pressed("move_right"):
+		$AnimatedPlayerSprite.play("lado")
+		scale.x = 1
 		direction.x += 1
 	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
 	if Input.is_action_pressed("move_left"):
+		$AnimatedPlayerSprite.play("lado")
+		scale.x = -1
 		direction.x -= 1
 		
 	if direction == Vector2.ZERO:
 		$AnimatedPlayerSprite.pause()
-	else:
-		$AnimatedPlayerSprite.play()
 	
 	position += direction.normalized() * delta * 200
